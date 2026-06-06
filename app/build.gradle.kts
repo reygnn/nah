4c1707946157365174dcbe68a3aa8dfecbbd4c4d
user <reygnn@proton.me>
2026-06-06 15:03:27 +0200

    chore: v0.7.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.github.reygnn.nah"
         minSdk = 36
         targetSdk = 36
-        versionCode = 17
-        versionName = "0.7.7"
+        versionCode = 18
+        versionName = "0.7.8"
     }
 
     buildTypes {
